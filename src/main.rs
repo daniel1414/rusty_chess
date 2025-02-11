ee9d0044378de5fe628c13b704304b0efc64231b
Daniel <dcichon369@gmail.com>
2025-02-11 20:33:46 +0100

    Downloaded basic assets from rusty_engine and implemented the base example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+    health: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut game = Game::new();
+
+    let sprite = game.add_sprite("player", SpritePreset::RacingCarGreen);
+    sprite.scale = 2.0;
+    game.add_logic(game_logic);
+
+    let initial_game_state = GameState { health: 100 };
+    game.run(initial_game_state);
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    let player = engine.sprites.get_mut("player").unwrap();
+
+    player.rotation += std::f32::consts::PI * engine.delta_f32;
+
+    if player.translation.x > 100.0 {
+        game_state.health -= 1;
+    }
 }
